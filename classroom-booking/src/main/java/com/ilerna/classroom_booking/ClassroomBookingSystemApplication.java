c1c1800e8fc0d394c612e53d4b6ec7511977a77f
Ángel Luis Matador Posada <angelluismatadorposada@gmail.com>
2025-06-03 15:33:41 +0200

    Database

---
+++ classroom-booking/src/main/java/com/ilerna/classroom_booking/ClassroomBookingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.ilerna.classroom_booking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClassroomBookingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClassroomBookingSystemApplication.class, args);
+	}
+
+}
---
+++ classroom-booking/src/test/java/com/ilerna/classroom_booking/ClassroomBookingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ilerna.classroom_booking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClassroomBookingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
