570d829e881234eb3bce91039cab0be7a5438955
True-Felas <juan.jimenez.nieto1@gmail.com>
2025-06-06 17:31:43 +0200

    Co-authored-by: Ángel Luis Matador Posada <angelluismatadorposada@gmail.com>

--- classroom-booking-frontend/src/app/app.routes.ts
+++ classroom-booking-frontend/src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { MapComponent } from './pages/map/map.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: 'home', pathMatch: 'full' },
     { path: 'home', component: HomeComponent },
-    { path: '*', redirectTo: 'home' }
+    { path: '*', redirectTo: 'home' },
+    { path: 'map', component: MapComponent }
 ];
