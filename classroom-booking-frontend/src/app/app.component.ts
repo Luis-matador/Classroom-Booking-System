e7afc64761778815ece2a13493b03b8fd526707c
BHMario <mariosanrui1612@gmail.com>
2025-06-06 11:57:42 +0200

    Estrucutra Frontend arreglada

--- classroom-booking-frontend/src/app/app.component.ts
+++ classroom-booking-frontend/src/app/app.component.ts
@@ -1,9 +1,20 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    HeaderComponent,
+    FooterComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
