5ba839710ed9adf3cd8f449e716dc59972ad87fe
True-Felas <juan.jimenez.nieto1@gmail.com>
2025-06-09 20:04:38 +0200

    Implementado mapa, falta arreglarlo

--- classroom-booking-frontend/src/app/pages/map/map.component.ts
+++ classroom-booking-frontend/src/app/pages/map/map.component.ts
@@ -1,18 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit, Inject, PLATFORM_ID } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
 
 @Component({
   selector: 'app-map',
-  imports: [],
   templateUrl: './map.component.html',
-  styleUrl: './map.component.css'
+  styleUrls: ['./map.component.css']
 })
-export class MapComponent {
-  goToNextFloor() {
-    const carousel = document.getElementById('carouselExampleIndicators');
-    // @ts-ignore
-    if (carousel && window.bootstrap) {
-      // @ts-ignore
-      window.bootstrap.Carousel.getOrCreateInstance(carousel).next();
+export class MapComponent implements AfterViewInit {
+  private map!: any;
+
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) {}
+
+  ngAfterViewInit(): void {
+    if (isPlatformBrowser(this.platformId)) {
+      this.initMap();
     }
+  }
+
+  private async initMap(): Promise<void> {
+    const L = await import('leaflet'); 
+
+    this.map = L.map('map', {
+      crs: L.CRS.Simple,
+      minZoom: -2,
+      maxZoom: 2
+    });
+
+    const bounds: L.LatLngBoundsExpression = [[0, 0], [800, 1000]];
+    L.imageOverlay('assets/planta1-1.png', bounds).addTo(this.map);
+    this.map.fitBounds(bounds);
+
+    L.marker([400, 500]).addTo(this.map).bindPopup('<b>Entrada principal</b>');
+    L.marker([600, 200]).addTo(this.map).bindPopup('Aula de Informática');
   }
 }
